7cf3f972c6a82ce4dfef1955e57a0046d7819270
skyislimit9218 <86927306+skyislimit9218@users.noreply.github.com>
2021-12-24 15:04:47 +0530

    Update Stress_Prediction.py

--- Stress_Prediction.py
+++ Stress_Prediction.py
@@ -1,10 +1,3 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB
 import pickle
 
 #cv = CountVectorizer()
